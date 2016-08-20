3ed309c340d0ff8b57bfb7280ae22753c8d481f7
Nathan Gaberel <nathang@theodo.co.uk>
2016-08-20 23:37:00 +0100

    Problem 11

---
+++ tests/test_problem11.py
@@ -0,0 +1,7 @@
+from problem11 import greatest_diagonal_product
+import unittest
+
+class TestProblem11(unittest.TestCase):
+
+    def test_problem(self):
+        self.assertEqual(greatest_diagonal_product(4), 70600674)
