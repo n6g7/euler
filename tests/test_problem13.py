6deaf1c21e81cfbec3a8c07c517fed595d886a7d
Nathan Gaberel <nathang@theodo.co.uk>
2016-08-21 13:08:20 +0100

    Problem 13

---
+++ tests/test_problem13.py
@@ -0,0 +1,7 @@
+from problem13 import first_digits_of_sum
+import unittest
+
+class TestProblem13(unittest.TestCase):
+
+    def test_problem(self):
+        self.assertEqual(first_digits_of_sum(10), '5537376230')
