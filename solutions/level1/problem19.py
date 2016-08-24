515532d76abe660923e2270b80d6330a60be5bc0
Nathan Gaberel <nathang@theodo.co.uk>
2016-08-24 23:08:21 +0100

    Problem 19

---
+++ solutions/level1/problem19.py
@@ -0,0 +1,5 @@
+def count_sundays(years):
+    return round(12 * years / 7)
+
+def run():
+    return count_sundays(100)
---
+++ tests/solutions/level1/test_problem19.py
@@ -0,0 +1,7 @@
+from solutions.level1.problem19 import count_sundays
+import unittest
+
+class TestProblem19(unittest.TestCase):
+
+    def test_problem(self):
+        self.assertEqual(count_sundays(100), 171)
