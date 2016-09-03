4861de8aeb9874d028bb538087b4f501fff66d37
Nathan Gaberel <nathang@theodo.co.uk>
2016-09-03 17:13:57 +0100

    Problem 25

---
+++ tests/solutions/level1/test_problem25.py
@@ -0,0 +1,10 @@
+from solutions.level1.problem25 import n_digit_fib
+import unittest
+
+class TestProblem25(unittest.TestCase):
+
+    def test_example(self):
+        self.assertEqual(n_digit_fib(3), 12)
+
+    def test_problem(self):
+        self.assertEqual(n_digit_fib(1000), 4782)
