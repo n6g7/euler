c90013076cadb904ddab9cbd46a6820d7ded9f4a
Nathan Gaberel <nathang@theodo.co.uk>
2016-09-04 21:41:55 +0100

    Problem 31

---
+++ solutions/level2/problem31.py
@@ -0,0 +1,25 @@
+from tools.digits import sum_digits
+
+def coin_sums(sum, coins):
+    cache = {}
+    def combinations(target, max_coin):
+        if (target, max_coin) not in cache:
+            if target == 0:
+                return 1
+
+            options = 0
+
+            available_coins = [c for c in coins if c <= max_coin]
+
+            for coin in available_coins:
+                if coin <= target:
+                    options += combinations(target-coin, coin)
+
+            cache[(target, max_coin)] = options
+
+        return cache[(target, max_coin)]
+
+    return combinations(sum, max(coins))
+
+def run():
+    return coin_sums(200, [1, 2, 5, 10, 20, 50, 100, 200])
---
+++ tests/solutions/level2/test_problem31.py
@@ -0,0 +1,7 @@
+from solutions.level2.problem31 import coin_sums
+import unittest
+
+class TestProblem31(unittest.TestCase):
+
+    def test_problem(self):
+        self.assertEqual(coin_sums(200, [1,2,5,10,20,50,100,200]), 73682)
