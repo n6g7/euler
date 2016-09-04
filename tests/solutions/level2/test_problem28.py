4f36fec9a90681c57182f680cf12a9407a51793e
Nathan Gaberel <nathang@theodo.co.uk>
2016-09-04 15:29:14 +0100

    Problem 28

---
+++ tests/solutions/level2/test_problem28.py
@@ -0,0 +1,10 @@
+from solutions.level2.problem28 import number_spiral_diags
+import unittest
+
+class TestProblem28(unittest.TestCase):
+
+    def test_example(self):
+        self.assertEqual(number_spiral_diags(5), 101)
+
+    def test_problem(self):
+        self.assertEqual(number_spiral_diags(1001), 669171001)
